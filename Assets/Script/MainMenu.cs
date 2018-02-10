2117905e589bd9ee91b4519204d6409b325e50be
Dian <diangli@usc.edu>
2018-02-10 11:36:45 -0800

    Dian

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+    public void PlayGame()
+    {
+        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void LoadByIndex(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit!");
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}
