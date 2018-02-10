804121d1dfe79efac5fa941b92aa9d4f5a6bd10a
a7114509 <pzengyi@gmail.com>
2018-02-10 11:16:58 -0800

    Add files via upload

---
+++ Assets/Scripts/Enermy.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enermy : MonoBehaviour {
+
+    public int hp = 100;
+    int swallowableBoundary = 80;
+    float moveSpeed = 0.02f;
+    float swallowableDistance = 1f;
+    GameObject player;
+
+
+    public void Die() {
+        hp -= 10;
+		if (hp < swallowableBoundary && Vector3.Distance (transform.position, player.transform.position) < swallowableDistance) {
+			Destroy(gameObject);
+			if(GameObject.Find("Player"))
+				GameObject.Find("Player").SendMessage("swallowanime");
+		
+		}
+
+    }
+
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.Find("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, moveSpeed);		
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+    private float moveSpeed = 6f;
+	bool Faceright=true;
+	Animator testwalk;
+	SpriteRenderer myspe;
+
+    // Use this for initialization
+    void Start () {
+		testwalk = GetComponent<Animator> ();
+		myspe = GetComponent<SpriteRenderer> ();
+
+
+
+		//triggerhp = Enermy
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float direction = CrossPlatformInputManager.GetAxis ("Horizontal");
+		if (direction > 0 && Faceright == false) {
+			Faceright = !Faceright;
+			myspe.flipX = false;
+		} else if (direction < 0 && Faceright == true) {
+			Faceright = !Faceright;
+			myspe.flipX = true;
+		}
+		GameObject theenermy = GameObject.Find("Enermy");
+		if (theenermy == null) {
+			testwalk.SetTrigger ("");
+		}
+
+
+
+
+        transform.Translate(moveSpeed * CrossPlatformInputManager.GetAxis("Horizontal") * Time.deltaTime, moveSpeed * CrossPlatformInputManager.GetAxis("Vertical") * Time.deltaTime, 0f);
+		if ((CrossPlatformInputManager.GetAxis ("Horizontal") + CrossPlatformInputManager.GetAxis ("Vertical")) == 0) {
+			testwalk.SetBool ("iswalk", false);
+
+		} else {
+			testwalk.SetBool ("iswalk", true);
+
+		}
+    }
+
+    public void Swallow()
+    {
+        if(GameObject.Find("Enermy"))
+            GameObject.Find("Enermy").SendMessage("Die");
+    }
+	public void swallowanime(){
+		testwalk.SetTrigger("Swa");
+	}
+}
