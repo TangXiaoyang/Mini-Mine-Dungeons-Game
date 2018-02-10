d127571f8c25fc96bf06972ba3774fdff94af1cb
kehuili <lkh12956@gmail.com>
2018-02-10 10:40:17 -0800

    gun

---
+++ Assets/Scripts/WaterballController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterballController : MonoBehaviour {
+
+	public float speed;
+	void Update () {
+		Vector3 worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 direction = (Vector2)((worldMousePos - transform.position));
+//		direction.Normalize ();
+		GetComponent<Rigidbody2D> ().velocity = speed * direction;
+
+		Destroy (gameObject ,0.3f);
+	}
+
+	void onTriggerEnter2D () {
+		Destroy (gameObject);
+	}
+}
